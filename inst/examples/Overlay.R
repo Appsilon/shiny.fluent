0a1ac663d10a340edd3e4a20c18d591a6ebda804
Marek Rogala <marek@appsilon.com>
2020-12-07 10:30:50 +0100

    Fix Overlay/Color picker examples

--- inst/examples/Overlay.R
+++ inst/examples/Overlay.R
@@ -3,12 +3,34 @@ library(shiny.fluent)
 if (interactive()) {
   shinyApp(
     ui = withReact(
-      Overlay(
-        isDarkThemed = TRUE,
-        "Inside Overlay"
+      div(
+        DefaultButton("toggleOverlay", text = "Open Overlay"),
+        reactOutput("overlay", height=NULL)
       )
     ),
     server = function(input, output) {
+      show <- reactiveVal(FALSE)
+      observeEvent(input$toggleOverlay, {
+        show(isolate(!show()))
+      })
+      output$overlay <- renderReact({
+        reactWidget(
+          if (show()) {
+            Overlay(
+              onClick=JS("function() { Shiny.setInputValue('toggleOverlay', Math.random()); }"),
+              isDarkThemed = TRUE,
+              div(
+                style="background: white; width: 50vw; height: 20rem; margin: auto;",
+                div(
+                  style="padding: 2rem;",
+                  h1("Inside Overlay"),
+                  p("Click anywhere to hide.")
+                )
+              )
+            )
+          }
+        )
+      })
     }
   )
 }
