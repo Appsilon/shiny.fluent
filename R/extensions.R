1d70d1c4146fa4d262115b249efe6e6153e018bc
Marek Rogala <marek@appsilon.com>
2021-02-23 16:54:44 +0100

    Add fluentPage.

--- R/extensions.R
+++ R/extensions.R
@@ -17,3 +17,30 @@ CommandBarItem <- function(text, icon = NULL, subitems = NULL, ...) {
   if (!is.null(subitems)) props$subMenuProps <- list(items = subitems)
   props
 }
+
+#' Basic Fluent UI page. Replacement for shiny::*Page family of functions.
+#'
+#' Creates a Fluent UI page with sensible defaults (included Fabric CSS classes, proper class given to the body tag,
+#' suppressed Bootstrap). You can also use shiny.fluent directly, without using this function.
+#' `bodyContent` does not get inserted as React to allow for flexibility. Use \code{\link{withReact}} or \code{\link{reactOutput}}
+#' to insert React content.
+#'
+#' @param bodyContent This argument will be inserted into the body tag directly.
+#' @param ... Additional tags that will be inserted into the page. Useful particularly for including head tag content like CSS.
+#' @export
+fluentPage <- function(bodyContent, ...){
+  fluidPage(
+    suppressDependencies("bootstrap"),
+    ...,
+    htmltools::htmlDependency(
+      "office-ui-fabric-core",
+      "11.0.0",
+      list(href="https://static2.sharepointonline.com/files/fabric/office-ui-fabric-core/11.0.0/css/"),
+      stylesheet = "fabric.min.css"
+    ),
+    shiny::tags$body(
+      class = "ms-Fabric",
+      bodyContent
+    )
+  )
+}
