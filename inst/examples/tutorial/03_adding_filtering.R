606bba552bd2e21ec2437c3f26fe5297a79d25b0
Kamil Zyla <kamil@appsilon.com>
2021-03-09 13:15:10 +0100

    Add code & screenshot for part 3: adding filtering

--- inst/examples/tutorial/03_adding_filtering.R
+++ inst/examples/tutorial/03_adding_filtering.R
@@ -2,24 +2,37 @@ library(shiny)
 library(shiny.fluent)
 library(dplyr)
 
-# ---- details_list_columns ----
+# ---- 03_filters_ui
+filters <- tagList(
+  DatePicker("fromDate", value = as.Date('2020/01/01'), label = "From date"),
+  DatePicker("toDate", value = as.Date('2020/12/31'), label = "To date")
+)
+# ----
+
 details_list_columns <- tibble(
   fieldName = c("rep_name", "date", "deal_amount", "client_name", "city", "is_closed"),
   name = c("Sales rep", "Close date", "Amount", "Client", "City", "Is closed?"),
   key = fieldName)
 
-# ---- deals_table ----
+# ---- 03_ui
 ui <- fluentPage(
   withReact(
+    filters,
     uiOutput("analysis")
   )
 )
+# ----
 
+# ---- 03_filters_server
 server <- function(input, output, session) {
   filtered_deals <- reactive({
-    filtered_deals <- fluent_sales_deals %>% filter(is_closed > 0)
+    filtered_deals <- fluent_sales_deals %>% filter(
+      date >= input$fromDate,
+      date <= input$toDate,
+      is_closed > 0
+    )
   })
-
+# ----
   output$analysis <- renderUI({
     items_list <- if(nrow(filtered_deals()) > 0){
       DetailsList(items = filtered_deals(), columns = details_list_columns)
