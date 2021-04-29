285b2b8ca039109237e581e0ce39da687e31c978
Jakub Sobolewski <jakub.sobolewski@appsilon.com>
2021-04-29 14:14:37 +0200

    Removed duplicated files

--- inst/examples/e2e-test/utils.R
+++
@@ -1,8 +0,0 @@
-wireInputToOutput <- function(ids, input, output) {
-  lapply(ids, function(id) {
-    output[[paste0(id, "Value")]] <- renderText({
-      v <- input[[id]]
-      sprintf("Value: %s", if (is.list(v)) v$key else v)
-    })
-  })
-}
