25e1e882323e6e5c65faa878b2fc04fd1bce82f9
Jakub Sobolewski <jakub.sobolewski@appsilon.com>
2021-04-29 09:34:01 +0200

    Combined '001_inputs' and '002_more_inputs' apps with 'e2e-test' app

--- inst/examples/e2e-test/app.R
+++ inst/examples/e2e-test/app.R
@@ -1,88 +1,20 @@
 library(shiny)
 library(shiny.fluent)
 
+source("utils.R")
+source("toggle_visibility.R")
+source("scenarios.R")
+
 ui <- fluidPage(
-  h1("Inputs tests"),
-  actionButton("updateInputs", "Update"),
-  actionButton("toggle", "Toggle visibility"),
-  uiOutput("main"),
-  h1("UI Components tests"),
-  Stack(
-    div("placeholder")
-  ),
-  h1("Scenarios tests"),
-  div(
-    Pivot(
-      PivotItem(headerText = "Tab 1", Label("Hello 1")),
-      PivotItem(headerText = "Tab 2", Label("Hello 2")),
-      onLinkClick = JS("function() { alert('Pivot tab switched') }")
-    ),
-    Pivot(
-      PivotItem(headerText = "Tab 1", Label("Hello 1")),
-      PivotItem(headerText = "Initially hidden components",
-        div(
-          textInput("scenarioInput1", "Label"),
-          textOutput("scenarioTextOutput1")
-        )
-      )
-    )
-  ),
-  h2("Update button inside React"),
-  Stack(
-    div(
-      actionButton("updateInputs2", "Update text"),
-      TextField.shinyInput("scenarioInput2"),
-      textOutput("scenarioTextOutput2")
-    )
-  )
+  toggleVisibilityUI("toggleVisibility"),
+  scenariosUI("scenarios")
 )
 
 server <- function(input, output, session) {
-  show <- reactiveVal(TRUE)
-  observeEvent(input$toggle, {
-    show(!show())
-  })
-
-  output$main <- renderUI({
-    if (show()) {
-      div(
-        h4("TextField"),
-        TextField.shinyInput("input1"),
-        textOutput("textOutput1"),
-        h4("textAreaInput"),
-        textAreaInput("input2", NULL),
-        textOutput("textOutput2"),
-        h4("checkboxInput"),
-        checkboxInput("input3", NULL),
-        textOutput("textOutput3")
-      )
-    } else {
-      NULL
-    }
-  })
-
-  wireInputToOutput <- function(inputPrefix, outputPrefix){
-    function(i) {
-      inputName <- paste0(inputPrefix, i)
-      outputName <- paste0(outputPrefix, i)
-      output[[outputName]] <- renderText({
-        sprintf("Value: %s", input[[inputName]])
-      })
-    }
-  }
-
-  lapply(1:3, wireInputToOutput("input", "textOutput"))
-  lapply(1:2, wireInputToOutput("scenarioInput", "scenarioTextOutput"))
-
-  observeEvent(input$updateInputs, {
-    updateTextField.shinyInput(session, "input1", value = paste(input$input1, "new text"))
-    updateTextAreaInput(session, "input2", value = paste(input$input1, "new text"))
-    updateCheckboxInput(session, "input3", value = FALSE)
-  })
-
-  observeEvent(input$updateInputs2, {
-    updateTextField.shinyInput(session, "scenarioInput2", value = "new text")
-  })
+  
+  toggleVisibilityServer("toggleVisibility")
+  scenariosServer("scenarios")
+  
 }
 
 shinyApp(ui, server)
---
+++ inst/examples/e2e-test/utils.R
@@ -0,0 +1,8 @@
+wireInputToOutput <- function(ids, input, output) {
+  lapply(ids, function(id) {
+    output[[paste0(id, "Value")]] <- renderText({
+      v <- input[[id]]
+      sprintf("Value: %s", if (is.list(v)) v$key else v)
+    })
+  })
+}
