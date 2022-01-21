ff1b2428fb77f48e616025a88316da44b5d017cb
Jakub Sobolewski <jakub.sobolewski@appsilon.com>
2022-01-21 10:54:12 +0100

    Add E2E tests

---
+++ inst/examples/e2e-test/R/themeProvider.R
@@ -0,0 +1,45 @@
+options <- list(
+  list(key = "A", text = "Option A"),
+  list(key = "B", text = "Option B")
+)
+theme <- list(
+  palette = list(
+    themePrimary = "#8dd400",
+    themeLighterAlt = "#060800",
+    themeLighter = "#172200",
+    themeLight = "#2a3f00",
+    themeTertiary = "#557f00",
+    themeSecondary = "#7cba00",
+    themeDarkAlt = "#97d816",
+    themeDark = "#a6de35",
+    themeDarker = "#bce766",
+    neutralLighterAlt = "#323130",
+    neutralLighter = "#31302f",
+    neutralLight = "#2f2e2d",
+    neutralQuaternaryAlt = "#2c2b2a",
+    neutralQuaternary = "#2a2928",
+    neutralTertiaryAlt = "#282726",
+    neutralTertiary = "#c8c8c8",
+    neutralSecondary = "#d0d0d0",
+    neutralPrimaryAlt = "#dadada",
+    neutralPrimary = "#ffffff",
+    neutralDark = "#f4f4f4",
+    black = "#f8f8f8",
+    white = "#323130"
+  )
+)
+
+themeProviderUI <- function(id) {
+  ns <- NS(id)
+  div(
+    `data-cy` = ns("themeProviderContent"),
+    ThemeProvider(
+      theme = theme,
+      Stack(
+        tokens = list(childrenGap = "10px"),
+        style = list(width = 250),
+        PrimaryButton(text = "PrimaryButton", `data-cy` = ns("button"))
+      )
+    )
+  )
+}
---
+++ js/cypress/integration/themeProvider.js
@@ -0,0 +1,23 @@
+describe('ShimmeredDetailsList()', () => {
+  const backgroundColor = 'rgb(50, 49, 48)';
+  const primaryColor = 'rgb(141, 212, 0)';
+  
+  before(() => {
+    cy.visit('/');
+  });
+
+  it('should set background color', () => {
+    cy.get('[data-cy="themeProvider-themeProviderContent"]').within(() => {
+      cy.get('div[class^="body-"]')
+        .should('have.css', 'background-color', backgroundColor);
+    });
+  });
+
+  it('should set style of an ActionButton', () => {
+    cy.get('[data-cy="themeProvider-themeProviderContent"]').within(() => {
+      cy.get('button[data-cy="themeProvider-button"]')
+        .should('have.css', 'background-color', primaryColor)
+        .and('have.css', 'color', backgroundColor);
+    });
+  });
+});
