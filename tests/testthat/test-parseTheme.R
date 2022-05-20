b3adfd214624b39cd4c052bb9c29436723dc97e5
Jakub Sobolewski <jakub.sobolewski@appsilon.com>
2022-05-20 10:23:50 +0200

    Use 'path' and 'json' argumuments in 'parseTheme' function

--- tests/testthat/test-parseTheme.R
+++ tests/testthat/test-parseTheme.R
@@ -1,8 +1,28 @@
 describe("parseTheme", {
-  it("should read the theme json and return a list", {
-    jsonString <- "{\n\"themePrimary\":\"black\"}"
+  it("throws an error if both 'path' and 'json' are supplied", {
+    expect_error(
+      parseTheme(path = "theme.json", json = "{}"),
+      regexp = "Only one 'path' or 'json' argument must be used'"
+    )
+  })
+
+  it("throws an error if neither 'path' nor 'json' is supplied", {
+    expect_error(parseTheme(), regexp = "Provide 'path' or 'json' argument")
+  })
+
+  it("throws an error if an invalid JSON is passed to the function", {
+    json <- '{\n"themePrimary":}'
+    expect_error(parseTheme(json = json))
+  })
+
+  it("throws an error if theme JSON doesn't exist", {
+    expect_error(suppressWarnings(parseTheme(path = "theme.json")))
+  })
+
+  it("reads the theme json and return a list", {
+    json <- '{\n"themePrimary":"black"}'
     expect_equal(
-      parseTheme(jsonString),
+      parseTheme(json = json),
       list(
         palette = list(
           themePrimary = "black"
@@ -11,20 +31,11 @@ describe("parseTheme", {
     )
   })
 
-  it("should throw an error if invalid JSON is passed to the function", {
-    jsonString <- "{\n\"themePrimary\":}"
-    expect_error(parseTheme(jsonString))
-  })
-
-  it("should throw an error if theme JSON doesn't exist", {
-    expect_error(parseTheme("theme.json"))
-  })
-
-  it("should read the theme json and return a list", {
+  it("reads the theme json and return a list", {
     withr::with_tempdir({
-      cat("{\n\"themePrimary\":\"black\"}", file = "theme.json")
+      cat('{\n"themePrimary":"black"}', file = "theme.json")
       expect_equal(
-        parseTheme("theme.json"),
+        parseTheme(path = "theme.json"),
         list(
           palette = list(
             themePrimary = "black"
