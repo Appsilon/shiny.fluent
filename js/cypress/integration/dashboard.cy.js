1ca221aceeaa59daa433ca76c4df68a4b4c11a28
Ivan Hrychaniuk <vanhry@gmail.com>
2023-03-29 11:50:06 +0300

    linter fix

--- js/cypress/integration/dashboard.cy.js
+++ js/cypress/integration/dashboard.cy.js
@@ -1,14 +1,17 @@
-
 describe('Dashboard', () => {
-    it('Slider', () => { 
-        cy.visit('#!/Slider');
-        const steps = 15
-        const arrows = '{rightarrow}'.repeat(steps); 
-        
-        cy.get('.ms-Slider-slideBox')
-        .focus()
-        .type(arrows);
-
-        cy.contains('Value: 15');
-    });
-})
+  it('Slider', () => {
+    cy.visit('#!/Slider');
+    const steps = 15;
+    const arrows = '{rightarrow}'.repeat(steps);
+    cy.get('.ms-Slider-slideBox')
+      .focus()
+      .type(arrows);
+    cy.contains('Value: 15');
+  });
+  it('Dropdown', () => {
+    cy.visit('#!/Dropdown');
+    cy.get('.ms-Dropdown-title').click();
+    cy.get('.ms-Dropdown-item').contains('Option C').click();
+    cy.contains('Value: C');
+  });
+});
