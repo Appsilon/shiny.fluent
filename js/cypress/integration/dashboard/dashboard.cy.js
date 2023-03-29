7c5c466e8a714eb7d89b7295cdd2e5eea898e050
Ivan Hrychaniuk <vanhry@gmail.com>
2023-03-29 13:34:13 +0300

    Separate folders for regular tests and demo dashboard tests. Use different ports

--- js/cypress/integration/dashboard/dashboard.cy.js
+++ js/cypress/integration/dashboard/dashboard.cy.js
@@ -1,6 +1,6 @@
 describe('Dashboard', () => {
   it('Slider', () => {
-    cy.visit('#!/Slider');
+    cy.visit('http://localhost:8889/#!/Slider');
     const steps = 15;
     const arrows = '{rightarrow}'.repeat(steps);
     cy.get('.ms-Slider-slideBox')
@@ -9,7 +9,7 @@ describe('Dashboard', () => {
     cy.contains('Value: 15');
   });
   it('Dropdown', () => {
-    cy.visit('#!/Dropdown');
+    cy.visit('http://localhost:8889/#!/Dropdown');
     cy.get('.ms-Dropdown-title').click();
     cy.get('.ms-Dropdown-item').contains('Option C').click();
     cy.contains('Value: C');
