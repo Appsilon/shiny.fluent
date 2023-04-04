17901315221c97de8a681550dff8a1081a4f93be
Ivan Hrychaniuk <vanhry@gmail.com>
2023-04-04 17:23:49 +0300

    Remove dependencies of shiny.router in demo

--- inst/examples/tutorial/dashboard_layout.R
+++ inst/examples/tutorial/dashboard_layout.R
@@ -270,21 +270,15 @@ ui <- fluentPage(
 
 router <- router_ui(
   route("/", home_page),
-  route("other", analysis_page))
+  route("other", analysis_page)
+)
 
 # ---- router-ui ----
-
-# Add shiny.router dependencies manually: they are not picked up because they're added in a non-standard way.
-shiny::addResourcePath("shiny.router", system.file("www", package = "shiny.router"))
-shiny_router_js_src <- file.path("shiny.router", "shiny.router.js")
-shiny_router_script_tag <- shiny::tags$script(type = "text/javascript", src = shiny_router_js_src)
-
 
 ui <- fluentPage(
   layout(router),
   tags$head(
-    tags$link(href = "style.css", rel = "stylesheet", type = "text/css"),
-    shiny_router_script_tag
+    tags$link(href = "style.css", rel = "stylesheet", type = "text/css")
   ))
 
 
