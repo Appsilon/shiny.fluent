f354274ba5141a431e3d199fdd9f13960e755a2a
Jakub Sobolewski <jakub.sobolewski@appsilon.com>
2023-06-30 14:37:20 +0200

    fix: don't run examples during package check

--- inst/examples/Button3.R
+++ inst/examples/Button3.R
@@ -7,41 +7,48 @@ library(shinyjs)
 # This example app shows how to use a Fluent UI Button to trigger a file upload.
 # File upload is not natively supported by shiny.fluent, so shinyjs is used
 # to trigger the file upload input.
-ui <- fluentPage(
-  useShinyjs(),
-  Stack(
-    tokens = list(
-      childrenGap = 10L
-    ),
-    horizontal = TRUE,
-    DefaultButton.shinyInput(
-      inputId = "uploadFileButton",
-      text = "Upload File",
-      iconProps = list(iconName = "Upload")
-    ),
-    div(
-      style = "
-        visibility: hidden;
-        height: 0;
-        width: 0;
-      ",
-      fileInput(
-        inputId = "uploadFile",
-        label = NULL
+ui <- function(id) {
+  ns <- NS(id)
+  fluentPage(
+    useShinyjs(),
+    Stack(
+      tokens = list(
+        childrenGap = 10L
+      ),
+      horizontal = TRUE,
+      DefaultButton.shinyInput(
+        inputId = ns("uploadFileButton"),
+        text = "Upload File",
+        iconProps = list(iconName = "Upload")
+      ),
+      div(
+        style = "
+          visibility: hidden;
+          height: 0;
+          width: 0;
+        ",
+        fileInput(
+          inputId = ns("uploadFile"),
+          label = NULL
+        )
       )
-    )
-  ),
-  textOutput("file_path")
-)
+    ),
+    textOutput("file_path")
+  )
+}
 
-server <- function(input, output) {
-  observeEvent(input$uploadFileButton, {
-    click("uploadFile")
-  })
+server <- function(id) {
+  moduleServer(id, function(input, output, session) {
+    observeEvent(input$uploadFileButton, {
+      click("uploadFile")
+    })
 
-  output$file_path <- renderText({
-    input$uploadFile$name
+    output$file_path <- renderText({
+      input$uploadFile$name
+    })
   })
 }
 
-shinyApp(ui, server)
+if (interactive()) {
+  shinyApp(ui("app"), function(input, output) server("app"))
+}
--- inst/examples/Button4.R
+++ inst/examples/Button4.R
@@ -7,32 +7,39 @@ library(shinyjs)
 # This example app shows how to use a Fluent UI Button to trigger a file download.
 # File download is not natively supported by shiny.fluent, so shinyjs is used
 # to trigger the file upload input.
-ui <- fluentPage(
-  useShinyjs(),
-  DefaultButton.shinyInput(
-    "downloadButton",
-    text = "Download",
-    iconProps = list(iconName = "Download")
-  ),
-  div(
-    style = "visibility: hidden;",
-    downloadButton("download", label = "")
-  )
-)
-
-server <- function(input, output, session) {
-  observeEvent(input$downloadButton, {
-    click("download")
-  })
-
-  output$download <- downloadHandler(
-    filename = function() {
-      paste("data-", Sys.Date(), ".csv", sep="")
-    },
-    content = function(file) {
-      write.csv(iris, file)
-    }
+ui <- function(id) {
+  ns <- NS(id)
+  fluentPage(
+    useShinyjs(),
+    DefaultButton.shinyInput(
+      inputId = ns("downloadButton"),
+      text = "Download",
+      iconProps = list(iconName = "Download")
+    ),
+    div(
+      style = "visibility: hidden;",
+      downloadButton(ns("download"), label = "")
+    )
   )
 }
 
-shinyApp(ui, server)
+server <- function(id) {
+  moduleServer(id, function(input, output, session) {
+    observeEvent(input$downloadButton, {
+      click("download")
+    })
+
+    output$download <- downloadHandler(
+      filename = function() {
+        paste("data-", Sys.Date(), ".csv", sep="")
+      },
+      content = function(file) {
+        write.csv(iris, file)
+      }
+    )
+  })
+}
+
+if (interactive()) {
+  shinyApp(ui("app"), function(input, output) server("app"))
+}
